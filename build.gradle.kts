eee25a7ea5ebbc2db153b50961b63c3252887753
kanake10 <10kanake@gmail.com>
2022-10-16 04:42:58 +0300

    replace plugins with newely created vals in respective objects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.2.1" apply false
-    id("com.android.library") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.5.0-alpha01" apply false
-    id("com.google.dagger.hilt.android") version "2.41" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
-    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
+    id(BuildPlugins.androidApplication) version "7.2.1" apply false
+    id(BuildPlugins.AndroidLibrary) version "7.2.1" apply false
+    id(BuildPlugins.kotlinJetBrains) version "1.6.10" apply false
+    id(BuildPlugins.NavSafeArgsKt) version "2.5.0-alpha01" apply false
+    id(BuildPlugins.Hilt) version "2.41" apply false
+    id(BuildPlugins.ktlint) version "10.2.1"
+    id(BuildPlugins.kotlinJvm) version "1.6.10" apply false
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
-
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    // Version should be inherited from parent
     repositories {
         // Required to download KtLint
         mavenCentral()
